45fcf1d78311edfc40d60d5e05a92c7636e3a9d5
Limiana <5073202+Limiana@users.noreply.github.com>
2023-05-03 00:17:00 +0300

    strip out overcomplicated stuff

--- Pal.Client/Net/RemoteApi.cs
+++ Pal.Client/Net/RemoteApi.cs
@@ -11,9 +11,9 @@ namespace Pal.Client.Net
     internal sealed partial class RemoteApi : IDisposable
     {
 #if DEBUG
-        public const string RemoteUrl = "https://api.puni.sh/palacepal";
+        public const string RemoteUrl = "https://api.puni.sh";
 #else
-        public const string RemoteUrl = "https://api.puni.sh/palacepal";
+        public const string RemoteUrl = "https://api.puni.sh";
 #endif
         private readonly string _userAgent =
             $"{typeof(RemoteApi).Assembly.GetName().Name?.Replace(" ", "")}/{typeof(RemoteApi).Assembly.GetName().Version?.ToString(2)}";
